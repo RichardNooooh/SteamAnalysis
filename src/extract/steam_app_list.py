33fcce05a96ccb02b1a2695ae4868dc200930e52
RichardNooooh <richard.jun.noh@gmail.com>
2024-10-17 09:14:33 -0500

    Refactor steam_app_list.py

--- src/extract/steam_app_list.py
+++ src/extract/steam_app_list.py
@@ -4,23 +4,44 @@ from api_scraper import APIScraper
 import json
 
 class SteamAppList(APIScraper):
+    """
+    Retrieves all game and DLC app IDs on Steam, not including ones still in Early Access
+    or exclusively available outside of the US. This list will be used for future interactions with
+    the app detail retrievals from the Steam API and Gamalytic.
+
+    This is done in two steps:
+    1. Retrieve the raw list of Steam Apps from `https://api.steampowered.com/ISteamApps/GetAppList/v2/`
+        - This data includes Games/DLCs, but also contains Demos, "Videos", Soundtracks, and more.
+        - For whatever reason, sometimes returns duplicates...
+    2. Filter out nonvisible IDs and non-game related apps with `https://api.steampowered.com/IStoreBrowseService/GetItems/v1`
+    """
     def __init__(self):
         super().__init__("https://api.steampowered.com/ISteamApps/GetAppList/v2/")
-        self.app_id_file = "../../data/raw/steam_ids/all_app_ids.txt"
+        self.raw_id_file = "../../data/raw/steam_ids/_ids_raw.txt"
 
         self.item_URL = "https://api.steampowered.com/IStoreBrowseService/GetItems/v1"
         self.game_id_file = "../../data/raw/steam_ids/game_ids.txt"
         self.dlc_id_file =  "../../data/raw/steam_ids/dlc_ids.txt"
 
         self.query = {"key": self.STEAM_API_KEY}
-        self.log = logging.getLogger(__name__)
+        self.filter_query = {
+            "ids": None,
+            "context": {
+                "language": "english",
+                "country_code": "US",
+                "steam_realm": 1,
+            }
+        }
+
         self.REQUEST_INTERVAL_TIME = 1.0
+
 
     def get_app_list(self) -> None:
         """
-        Fetches list of all Steam app IDs with GET request to API. Writes to `self.app_id_file`
-        Uses API key since it may or may not affect the output... (hard to tell)
+        Fetches list of all Steam app IDs with GET request to API. Writes to `self.raw_id_file`
+        Uses API key since it may or may not affect the output.
         """
+        self.log.info("Beginning Steam app ID data retrieval")
         response = self.get_request(
             self.BASE_URL, 1, params=self.query, headers=self.headers
         )
@@ -34,72 +55,82 @@ class SteamAppList(APIScraper):
                 f"Found {len(app_data) - len(set(app_data))} duplicates in the list"
             )
 
-        self.log.info(f"Writing data into {self.app_id_file}")
-        with open(self.app_id_file, mode="w") as f:
+        self.log.info(f"Writing data into {self.raw_id_file}")
+        with open(self.raw_id_file, mode="w") as f:
             for app in app_data:
                 f.write(f"{app[0]}\t{app[1]}\n")
         self.log.info("Successfully finished writing all raw app ids")
 
+
+    def process_batch(self, store_items: dict, names_batch: dict, game_ids: list, dlc_ids: list) -> None:
+        """
+        Using the data from `store_items`, append IDs (and corresponding names from `names_batch`)
+        to `game_ids` and `dlc_ids`.
+
+        Keep in mind the API can change at any point.
+        """
+        for item in store_items:
+            name = names_batch[item["appid"]]
+            if not item["visible"]:
+                self.log.warning(f"Item ID {item["appid"]} (name={name}) is not visible")
+                continue
+            
+            if "type" not in item:
+                self.log.warning(f"Visible app {item["appid"]} (name={name}) does not have `type` field")
+                continue
+            
+            if item["type"] == 0: # Games
+                game_ids.append((item["appid"], name))
+            elif item["type"] == 4: # DLC
+                dlc_ids.append((item["appid"], name))
+            else:
+                self.log.debug(f"Filtered out {item["appid"]} (name={name})")
+
+
     def filter_app_list(self, batch_size: int = 100) -> None:
         """
-        Using IDs from `self.app_id_file`, filter games (type=0) and DLC (type=4) items and record
+        Using IDs from `self.raw_id_file`, filter games (type=0) and DLC (type=4) items and record
         them into `self.game_id_file` and `self.dlc_id_file`.
+
+        This is obtained with a GET request of multiple app IDs to:
+        `https://api.steampowered.com/IStoreBrowseService/GetItems/v1`.
+
+        This API endpoint requires a single query parameter, `input_json`.
 
         Args:
             batch_size (int): number of app ids per request
         """
-        # read app ids
+        self.log.info("Beginning filtering process")
         app_ids = []
-        with open(self.app_id_file, "r") as f:
-            app_ids = [line.strip().split("\t") for line in f.readlines()]
-
-        input_json = {
-            "ids": None,
-            "context": {
-                "language": "english",
-                "country_code": "US",
-                "steam_realm": 1,
-            }
-        }
+        with open(self.raw_id_file, "r") as f:
+            for app in f:
+                data = app.split("\t")
+                app_ids.append((int(data[0]), data[1].strip()))
+        self.log.info(f"Finished reading {len(app_ids)} app IDs from {self.raw_id_file}")
 
         total_apps = len(app_ids)
         game_ids, dlc_ids = [], []
+
         for i in range(0, total_apps, batch_size):
             if i % 1000 == 0:
                 self.log.info(f"Processed {i} apps")
 
             batch = app_ids[i : i + batch_size]
-            input_json["ids"] = [{"appid": app[0]} for app in batch]
+            self.filter_query["ids"] = [{"appid": app[0]} for app in batch]
             names_batch = {app[0]: (app[1] if len(app) == 2 else "") for app in batch}
 
-            response = self.get_request(self.item_URL, 3, params={"input_json": json.dumps(input_json)})
+            response = self.get_request(self.item_URL, 3, params={"input_json": json.dumps(self.filter_query)})
             self.log.debug(f"Successfully retrieved data from {response.url}")
             store_items = response.json().get("response", {}).get("store_items", [])
 
             if len(store_items) != len(batch):
                 self.log.warning(
                     f"Number of apps retrieved ({len(store_items)}) does "
-                    + f"not match number requested ({len(store_items)}) "
+                    + f"not match number requested ({len(batch)}) "
                     + f"in range {i}, {i+batch_size}"
                 )
 
-            for item in store_items:
-                name = names_batch[str(item["appid"])]
-                if not item["visible"]:
-                    self.log.warning(f"Item ID {item["appid"]} (name={name}) is not visible")
-                    continue
-                
-                if "type" not in item:
-                    self.log.warning(f"Visible app {item["appid"]} (name={name}) does not have `type` field")
-                    continue
-                
-                if item["type"] == 0: # Games
-                    game_ids.append((item["appid"], name))
-                elif item["type"] == 4: # DLC
-                    dlc_ids.append((item["appid"], name))
-                else:
-                    self.log.debug(f"Filtered out {item["appid"]} (name={name})")
-            
+            self.process_batch(store_items, names_batch, game_ids, dlc_ids)
             time.sleep(self.REQUEST_INTERVAL_TIME)
         
         self.log.info(f"Processed all apps: #Games={len(game_ids)}, #DLC={len(dlc_ids)}")
@@ -110,8 +141,6 @@ class SteamAppList(APIScraper):
         with open(self.dlc_id_file, mode="w") as f:
             for app in dlc_ids:
                 f.write(f"{app[0]}\t{app[1]}\n")
-
-        return
 
 
 if __name__ == "__main__":
